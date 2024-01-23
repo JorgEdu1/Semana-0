643d746a353e7a8fe9ec14f7ed92c5e040cba4b5
JorgEdu1 <ej33542@gmail.com>
2024-01-23 17:35:32 -0300

    mudando ano

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
             <a href="https://www.cajuinasaogeraldo.com.br/wordpress/politica-de-privacidade/"> Política de Privacidade</a>
             . Dúvidas? Ligue <a href="tel:1140036125">(11) 4003.6125</a>
             <br />
-            Cajuína São Geraldo, Todos os direitos reservados &copy; 2023
+            Cajuína São Geraldo, Todos os direitos reservados &copy; 2024
             <div className='credits'>
                 Criado com amor por <br /><a href="https://github.com/JorgEdu1">@JorgEdu1</a> e <a href="https://github.com/Kaynan951">@Kaynan951</a>
             </div>					
