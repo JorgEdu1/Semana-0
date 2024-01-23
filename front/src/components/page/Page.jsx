4ea02bd77d93a248ed867e2d77c90485864c6aa2
JorgEdu1 <ej33542@gmail.com>
2024-01-23 11:05:46 -0300

    configurando o projeto e colocando o bootstrap

---
+++ front/src/components/page/Page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+        <button type="button" class="btn btn-dark">Dark</button>
+    </div>
+  )
+}
+
+export default Page
